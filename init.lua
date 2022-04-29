6f060c7c0946d202651c25e4ed7bf736a4991d35
刘瑞 <liurui07@inspur.com>
2022-04-29 14:09:06 +0800

    cmd + alt +shift + d展示鼠标

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@
 
 require("config")
 require("window")
-
+require("applicationEvents")
 
 -- 按住cmd + alt + ctrl + w键,就会展示 Hello World, ,此时以通知的方式告知
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
